998c7305acfb98158bec0846571c79726ce3807b
Đỗ Bách <91376651+Do-Bach-2006@users.noreply.github.com>
2024-02-09 21:31:30 +0700

    Update getUserName.js

--- scripts/getUserName.js
+++ scripts/getUserName.js
@@ -5,24 +5,6 @@ function openLink() {
     const userName = document.getElementById('user_input').value;
 
     sessionStorage.setItem('userName' , userName);
-
-    // if( userName == special_troll_code)
-    // {
-    //     window.location.href = './troll.html';   
-
-    //     const troll_video = document.getElementById('troll_video');
-    //     troll_video.addEventListener('canplay', function() {
-    //         troll_video.play();
-    //     });
-    // }
-    // else 
-    // {
-    //     window.location.href = './test.html';
-    // }
-    
-    // sessionStorage.setItem('userName' , userName);
-
-    
 }
 
 let button = document.getElementById("input_button"); // Get the button element by its id
