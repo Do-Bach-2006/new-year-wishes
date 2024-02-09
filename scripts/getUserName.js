9b20367a3b91a0a64f111f1236eb5abcc9544d7a
Đỗ Bách <91376651+Do-Bach-2006@users.noreply.github.com>
2024-02-09 21:23:25 +0700

    Update getUserName.js

--- scripts/getUserName.js
+++ scripts/getUserName.js
@@ -1,26 +1,26 @@
 
 function openLink() {
     // Replace 'your-link.html' with the actual URL you want to open
-    // window.location.href = './test.html';
-    // const userName = document.getElementById('user_input').value;
+    window.location.href = './test.html';
+    const userName = document.getElementById('user_input').value;
 
-    // sessionStorage.setItem('userName' , userName);
-
-    if( userName == special_troll_code)
-    {
-        window.location.href = './troll.html';   
-
-        // const troll_video = document.getElementById('troll_video');
-        // troll_video.addEventListener('canplay', function() {
-        //     troll_video.play();
-        // });
-    }
-    else 
-    {
-        window.location.href = './test.html';
-    }
-    
     sessionStorage.setItem('userName' , userName);
+
+    // if( userName == special_troll_code)
+    // {
+    //     window.location.href = './troll.html';   
+
+    //     const troll_video = document.getElementById('troll_video');
+    //     troll_video.addEventListener('canplay', function() {
+    //         troll_video.play();
+    //     });
+    // }
+    // else 
+    // {
+    //     window.location.href = './test.html';
+    // }
+    
+    // sessionStorage.setItem('userName' , userName);
 
     
 }
